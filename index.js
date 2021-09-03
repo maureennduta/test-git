80b64bf3bf123ca3ad23a74a873a1dfab9200dea
Maureen Nduta <67265839+maureennduta@users.noreply.github.com>
2021-09-03 17:29:45 +0300

    Divides two values (#3)

--- index.js
+++ index.js
@@ -14,10 +14,16 @@ const moduls = (x, y) => {
   return x % y;
 }
 
+const div = (x, y) => {
+  return x / y
+}
+
 start();
 const sumOfTwo = add(3, 4);
 const minus = sub(90, 38);
 console.log(sumOfTwo, minus);
-const myModulus = moduls(7,4);
-console.log('Modulus',myModulus);
+const myModulus = moduls(7, 4);
+console.log('Modulus', myModulus);
+const divide = div(5000, 25);
+console.log(divide);
 
