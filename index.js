c3eef220605a37ac15916b55c52e3ab1b7ef5714
maikofelix47 <maikofelix89@gmail.com>
2021-09-03 17:19:13 +0300

    Added modulus function

--- index.js
+++ index.js
@@ -10,8 +10,14 @@ const sub = (x, y) => {
   return x - y;
 }
 
+const moduls = (x, y) => {
+  return x % y;
+}
+
 start();
 const sumOfTwo = add(3, 4);
 const minus = sub(90, 38);
 console.log(sumOfTwo, minus);
+const myModulus = moduls(7,4);
+console.log('Modulus',myModulus);
 
